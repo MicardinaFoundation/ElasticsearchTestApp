38d06e3f745adc4fc83f7a0a319427a5adb36e9b
Msk, Nikita <88129731+MicardinaFoundation@users.noreply.github.com>
2026-06-01 19:40:32 +0500

    practic-3? final (mb)

--- ElasticsearchTestApp/Program.cs
+++ ElasticsearchTestApp/Program.cs
@@ -15,6 +15,7 @@ namespace ElasticsearchTestApp
             // Add services to the container.
 
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
